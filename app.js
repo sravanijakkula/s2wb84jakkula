520e04d0519bfd96baa7e9f33f5d3bed0bca7042
Sravani Jakkula <sravanijakkula13@gmail.com>
2022-04-06 19:36:04 -0500

    updated reqyired files

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var fruitjuiceRouter = require('./routes/fruitjuice');
+var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/fruitjuice', fruitjuiceRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
